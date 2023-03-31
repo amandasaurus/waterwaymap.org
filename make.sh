e4534fed4d7b5df4e5f933df38386daaf30b5e91
Amanda McCann <amanda@technomancy.org>
2023-03-31 16:54:32 +0200

    misc

--- make.sh
+++ make.sh
@@ -5,10 +5,9 @@ cd "$(dirname "$0")"
 PREFIX="irl-br-waterway"
 TMP="${PREFIX}.$(date -u +%F.%s)"
 pyosmium-up-to-date -vv ~/osm-data/britain-and-ireland-latest.osm.pbf
-/home/amanda/code/rust/osm-lump-ways/target/release/osm-lump-ways -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river,stream,ditch --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
-#cargo run --release -- -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river,stream,ditch --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
-tippecanoe -y length_m -y root_wayid -l waterway --drop-smallest-as-needed --order-descending-by=length_m -o "${TMP}.pmtiles" "${TMP}.geojson"
+osm-lump-ways -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
+tippecanoe -S 10 -y length_m -y root_wayid -l waterway --drop-smallest-as-needed --order-descending-by=length_m -o "${TMP}.pmtiles" "${TMP}.geojson"
 
-mv "${TMP}.geojson" ./public_html/data/${PREFIX}.geojson
-gzip -9 ./public_html/data/${PREFIX}.geojson
-mv "${TMP}.pmtiles" ./public_html/tiles/${PREFIX}.pmtiles
+gzip -9 "${TMP}.geojson"
+mv -v "${TMP}.geojson.gz" "./docs/data/${PREFIX}.geojson.gz"
+mv -v "${TMP}.pmtiles" "./docs/tiles/${PREFIX}.pmtiles"
