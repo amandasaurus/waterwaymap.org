5ccba208cf2bc12a87d9797a2fe1564cc2e4efc6
Amanda McCann <amanda@technomancy.org>
2023-03-31 16:17:44 +0200

    misc

--- make.sh
+++ make.sh
@@ -5,7 +5,8 @@ cd "$(dirname "$0")"
 PREFIX="irl-br-waterway"
 TMP="${PREFIX}.$(date -u +%F.%s)"
 pyosmium-up-to-date -vv ~/osm-data/britain-and-ireland-latest.osm.pbf
-cargo run --release -- -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river,stream,ditch --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
+/home/amanda/code/rust/osm-lump-ways/target/release/osm-lump-ways -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river,stream,ditch --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
+#cargo run --release -- -i ~/osm-data/britain-and-ireland-latest.osm.pbf -o "${TMP}.geojson" -f waterway=river,stream,ditch --min-length-m 100 --timeout-dist-to-longer-s 0 --no-incl-wayids
 tippecanoe -y length_m -y root_wayid -l waterway --drop-smallest-as-needed --order-descending-by=length_m -o "${TMP}.pmtiles" "${TMP}.geojson"
 
 mv "${TMP}.geojson" ./public_html/data/${PREFIX}.geojson
