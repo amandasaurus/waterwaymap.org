b613dccea50940ef8739e97a0bcbcc130f5f4f79
Amanda McCann <amanda@technomancy.org>
2023-04-17 13:37:21 +0200

    misc

--- make-planet.sh
+++ make-planet.sh
@@ -2,6 +2,8 @@
 set -o errexit -o nounset
 cd "$(dirname "$0")"
 
+source functions.sh
+
 PREFIX="planet"
 TMP="${PREFIX}.$(date -u +%F.%s)"
 #pyosmium-up-to-date -vv ~/osm-data/planet-latest.osm.pbf || true
@@ -9,35 +11,6 @@ if [ planet-latest.osm.pbf -nt planet-waterway.osm.pbf ] ; then
 	echo "Data updates, rerunning osmium tags-filter"
 	osmium tags-filter --remove-tags --overwrite planet-latest.osm.pbf -o planet-waterway.osm.pbf waterway
 fi
-
-
-function process() {
-	INPUT=$1
-	PREFIX=$2
-	LUMP_ARGS=$3
-	FILE_TIMESTAMP=$(osmium fileinfo --get header.option.timestamp "$INPUT")
-	TMP="${PREFIX}.$(date -u +%F.%s)"
-
-	osm-lump-ways -i "$INPUT" -o "${TMP}.geojson" $LUMP_ARGS --min-length-m 100 --save-as-linestrings
-	echo "GeoJSON created successfully."
-	echo "Starting tippecanoe..."
-	tippecanoe \
-		-n "OSM River Topologies" \
-		-N "Generated on $(date -I) from OSM data from $FILE_TIMESTAMP with $(osm-lump-ways --version) and argument $LUMP_ARGS" \
-		-A "© OpenStreetMap. Open Data under ODbL. https://osm.org/copyright" \
-		-S 50 --single-precision \
-		--simplify-only-low-zooms \
-		-y length_m -y root_wayid \
-		-l waterway \
-		--gamma 2 \
-		--coalesce-smallest-as-needed \
-		--drop-smallest-as-needed --order-by=length_m \
-		-o "${TMP}.pmtiles" "${TMP}.geojson"
-	mv "${TMP}.geojson" "./docs/data/${PREFIX}.geojson"
-	mv "${TMP}.pmtiles" "./docs/tiles/${PREFIX}.pmtiles"
-	echo "GeoJSON & PMTiles created successfully."
-	gzip -f -9 "./docs/data/${PREFIX}.geojson" &
-}
 
 process colombia-waterway.osm.pbf colombia-waterway "-f waterway"
 process colombia-waterway.osm.pbf colombia-waterway-river "-f waterway=river"
