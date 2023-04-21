55bfc2a18ef50208abcce1376475c1a39f72aa76
Amanda McCann <amanda@technomancy.org>
2023-04-21 08:25:25 +0200

    misc

--- make-planet.sh
+++ make-planet.sh
@@ -6,20 +6,22 @@ source functions.sh
 
 PREFIX="planet"
 TMP="${PREFIX}.$(date -u +%F.%s)"
-#pyosmium-up-to-date -vv ~/osm-data/planet-latest.osm.pbf || true
 if [ planet-latest.osm.pbf -nt planet-waterway.osm.pbf ] ; then
 	echo "Data updates, rerunning osmium tags-filter"
-	osmium tags-filter --remove-tags --overwrite planet-latest.osm.pbf -o planet-waterway.osm.pbf waterway
+	TMP=$(mktemp -p . "tmp.planet.XXXXXX.osm.pbf")
+	osmium tags-filter --remove-tags --overwrite planet-latest.osm.pbf -o "$TMP" waterway
+	mv "$TMP" planet-waterway.osm.pbf
 fi
 
-process colombia-waterway.osm.pbf colombia-waterway "-f waterway"
-process colombia-waterway.osm.pbf colombia-waterway-river "-f waterway=river"
-process colombia-waterway.osm.pbf colombia-waterway-name-no-group "-f waterway -f name"
-process colombia-waterway.osm.pbf colombia-waterway-name-group-name "-f waterway -f name -g name"
-
 process ~/osm-data/planet-waterway.osm.pbf planet-waterway-river "-f waterway=river"
+rclone copyto ./docs/tiles/planet-waterway-river.pmtiles cloudflare:pmtiles0/2023-04-01/planet-waterway-river.pmtiles  --progress
+
 process ~/osm-data/planet-waterway.osm.pbf planet-waterway-name-no-group "-f waterway -f name"
+rclone copyto ./docs/tiles/planet-waterway-name-no-group.pmtiles cloudflare:pmtiles0/2023-04-01/planet-waterway-name-no-group.pmtiles  --progress
+
 process ~/osm-data/planet-waterway.osm.pbf planet-waterway-name-group-name "-f waterway -f name -g name"
+rclone copyto ./docs/tiles/planet-waterway-name-group-name.pmtiles cloudflare:pmtiles0/2023-04-01/planet-waterway-name-group-name.pmtiles  --progress
 
 wait
+
 exit 0
