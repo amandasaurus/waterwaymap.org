762ba6da668896bbe7b68837cc18eee0cbfa47e3
Amanda McCann <amanda@technomancy.org>
2023-12-07 23:28:49 +0100

    tmp: loops

---
+++ docs/tmp/loops/mapstyle.js
@@ -0,0 +1,20 @@
+var mapstyle_layers = [
+	{
+		"id": "osmcarto",
+		"type": "raster",
+		"source": "osmcarto"
+	},
+	{
+		"id":"waterway-line",
+		"type": "line",
+		"source": "waterway",
+		"layout": {
+			"line-cap":  "round",
+			"line-join":  "round"
+		},
+		"paint": {
+			"line-color":  "black",
+			"line-width": 5
+		}
+	}
+];
