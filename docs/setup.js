a93c6d7d5240d964bb96b3b6d0b38f88fa233cbe
Amanda McCann <amanda@technomancy.org>
2024-04-08 09:17:42 +0200

    if the tileset doesn't exist, default to defaul

--- docs/setup.js
+++ docs/setup.js
@@ -33,6 +33,10 @@ document.addEventListener("alpine:init", async () => {
   let params = new URLSearchParams((location.hash ?? "#").substr(1));
   let len_filter = decodeFilterParams(params.get("len") ?? "");
   let selected_tileset_key = params.get("tiles") ?? tilesets.selected_tileset;
+  if (!(selected_tileset_key in tilesets.tilesets)) {
+	  console.error("The selected tileset "+selected_tileset_key+" doesn't exist, using default of "+tilesets.selected_tileset+" instead.");
+	  selected_tileset_key = tilesets.selected_tileset;
+  }
   Alpine.store("selected_tileset", selected_tileset_key);
   Alpine.store("tilesets_loaded", true);
 
