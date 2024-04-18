12cbd1bb0a66bcb0c6719f335c1b630bcabbb380
mxdanger <32040254+mxdanger@users.noreply.github.com>
2024-04-17 17:46:17 -0700

    Display JOSM warning as a Bootstrap Toast

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,40 +1,46 @@
-const htmlElement = document.querySelector("html")
-if (htmlElement.getAttribute("data-bs-theme") === 'auto') {
-  function updateTheme() {
-    document.querySelector("html").setAttribute("data-bs-theme",
-      window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-  }
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
-  updateTheme()
+const editRemoteButton = document.getElementById('editRemoteButton')
+const editRemoteNewLayerButton = document.getElementById("editRemoteNewLayerButton");
+const toastContainer = document.getElementById('toastContainer')
+const josmToastTemplate = document.getElementById('josmToastTemplate').content.firstElementChild.cloneNode(true)
+
+if (editRemoteButton) {
+  editRemoteButton.addEventListener('click', () => {
+    remote()
+  })
 }
 
-const editRemoteButton = document.getElementById("editRemoteButton");
-
-editRemoteButton.addEventListener("click", function () {
-  finish();
-});
+if (editRemoteNewLayerButton) {
+  editRemoteNewLayerButton.addEventListener('click', () => {
+    remoteNewLayer()
+  })
+}
 
 async function remote() {
   let b = map.getBounds();
-  let url = `http://127.0.0.1:8111/load_and_zoom?top=${b.getNorth()}&amp;bottom=${b.getSouth()}&amp;left=${b.getWest()}&amp;right=${b.getEast()}&amp;changeset_source=${encodeURIComponent(
-    location.href
-  )}&amp;changeset_hashtags=${encodeURIComponent("#WaterwayMapOrg")}`;
-  let res_ok = await fetch(url).then((r) => r.ok).catch((e) => false);
+  let url = `http://127.0.0.1:8111/load_and_zoom?top=${b.getNorth()}&bottom=${b.getSouth()}&left=${b.getWest()}&right=${b.getEast()}&changeset_source=${encodeURIComponent(location.href)}&changeset_hashtags=${encodeURIComponent('#WaterwayMapOrg')}`;
+  let res_ok = await fetch(url).then((r) => r.ok).catch(() => false);
   this.disabled = false;
   if (!res_ok) {
-    show_josm_not_running = true;
-    setTimeout(() => (show_josm_not_running = false), 5000);
+    const josmToastEl = josmToastTemplate.cloneNode(true)
+    toastContainer.appendChild(josmToastEl)
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(josmToastEl)
+    toastBootstrap.show()
+    
   }
 }
 
-// let b = map.getBounds();
-// let url = `http://127.0.0.1:8111/load_and_zoom?new_layer=true&top=${b.getNorth()}&amp;bottom=${b.getSouth()}&amp;left=${b.getWest()}&amp;right=${b.getEast()}&amp;changeset_source=${encodeURIComponent(location.href)}&amp;changeset_hashtags=${encodeURIComponent('#WaterwayMapOrg')}`;
-// let res_ok = await fetch(url).then(r => r.ok).catch(e => false);
-// this.disabled = false;
-// if (!res_ok) {
-// 	show_josm_not_running = true;
-// 	setTimeout(() => show_josm_not_running = false, 5000);
-// }
+async function remoteNewLayer() {
+  let b = map.getBounds();
+  let url = `http://127.0.0.1:8111/load_and_zoom?new_layer=true&top=${b.getNorth()}&amp;bottom=${b.getSouth()}&amp;left=${b.getWest()}&amp;right=${b.getEast()}&amp;changeset_source=${encodeURIComponent(location.href)}&amp;changeset_hashtags=${encodeURIComponent('#WaterwayMapOrg')}}`;
+  let res_ok = await fetch(url).then((r) => r.ok).catch(() => false);
+  this.disabled = false;
+  if (!res_ok) {
+    const josmToastEl = josmToastTemplate.cloneNode(true)
+    toastContainer.appendChild(josmToastEl)
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(josmToastEl)
+    toastBootstrap.show()
+  }
+}
 
 document.querySelector("#shareButton").addEventListener("click", () => {
   if (navigator.share) {
