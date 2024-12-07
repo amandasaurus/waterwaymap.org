a9272887369d0a3a4e892b20cc551f408b932226
Amanda McCann <amanda@technomancy.org>
2024-12-07 09:51:32 +0100

    update version

--- make-planet.sh
+++ make-planet.sh
@@ -8,7 +8,7 @@ fi
 
 source functions.sh
 
-if [ "$(osm-lump-ways --version)" != "osm-lump-ways 2.1.0" ] ; then
+if [ "$(osm-lump-ways --version)" != "osm-lump-ways 2.2.0" ] ; then
   echo "Wrong version of osm-lump-ways installed: $(osm-lump-ways --version)"
   exit 1
 fi
