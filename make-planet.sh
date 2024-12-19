8c995b666cfb549806fcb247560908b95ce64063
Amanda McCann <amanda@technomancy.org>
2024-12-19 14:01:01 +0100

    create the upstream.csv

--- make-planet.sh
+++ make-planet.sh
@@ -52,6 +52,7 @@ make -j2 \
   planet-loops.pmtiles planet-loops-firstpoints.geojson.gz planet-ends.pmtiles planet-ends.geojson.gz \
   planet-loops.geojson.gz \
   planet-grouped-ends.pmtiles \
+  planet-upstreams.csv.zst \
   waterwaymap.org_ends_stats.csv.zst
 echo "Took $(units ${SECONDS}sec time) (${SECONDS}sec) to convert all geojsons to pmtiles"
 
