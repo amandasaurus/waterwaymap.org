4ff8e11f5ef45d95cf32e6984610553178f9ba6d
Amanda McCann <amanda@technomancy.org>
2025-05-31 21:57:47 +0200

    misc

--- riversite_input_data_setup.sql
+++ riversite_input_data_setup.sql
@@ -2,10 +2,9 @@ set client_min_messages = warning;    -- don't NOTICE if an index (etc) already 
 create index if not exists admins__level on admins (level);
 create index if not exists admins__name on admins (name);
 delete from admins where level IS NULL OR name IS NULL;
-create index if not exists admins__name on admins (name);
 
 -- merge things with same name & iso
-create temp table new_admins as select min(ogc_fid) as ogc_fid, name, iso, min(parent_iso) as parent_iso, level, ST_Union(geom) as geom from admins group by (name, iso, level);
+create temp table new_admins as select min(ogc_fid) as ogc_fid, name, iso, min(parent_iso) as parent_iso, min(level) as level, ST_Union(geom) as geom from admins group by (name, iso);
 truncate table admins cascade;
 insert into admins select * from new_admins;
 drop table new_admins;
@@ -20,12 +19,12 @@ alter table planet_grouped_waterways add constraint uniq_name_min_nid UNIQUE(tag
 alter table planet_grouped_waterways add column url_path text unique;
 update planet_grouped_waterways set url_path = coalesce(tag_group_value, 'unnamed') || ' ' || lpad(min_nid::text, 12, '0');
 
-drop table if exists ww_in_admin_ranks;
-create table ww_in_admin_ranks (
+create table IF NOT EXISTS ww_in_admin_ranks (
   a_ogc_fid integer references admins(ogc_fid),
   ww_ogc_fid integer references planet_grouped_waterways(ogc_fid),
   ww_rank_in_a bigint
 );
+TRUNCATE TABLE ww_in_admin_ranks;
 insert into ww_in_admin_ranks
   select
     a.ogc_fid as a_ogc_fid,
