3e84899eb6e7eb1486aa419efeb3a07dff805cfc
Amanda McCann <amanda@technomancy.org>
2025-05-31 18:59:38 +0200

    formating

--- riversite_input_data_setup.sql
+++ riversite_input_data_setup.sql
@@ -7,7 +7,16 @@ create index if not exists planet_grouped_waterways__name on planet_grouped_wate
 
 drop table if exists ww_rank_in_a;
 create table ww_in_admin_ranks AS 
-  select a.ogc_fid as a_ogc_fid, ww.ogc_fid as ww_ogc_fid, RANK() OVER (partition by a.ogc_fid order by ww.length_m desc) as ww_rank_in_a from planet_grouped_waterways as ww join admins as a on ST_Intersects(a.geom, ww.geom);
+  select
+    a.ogc_fid as a_ogc_fid,
+    ww.ogc_fid as ww_ogc_fid,
+    RANK() OVER (partition by a.ogc_fid order by ww.length_m desc) as ww_rank_in_a
+  FROM
+    planet_grouped_waterways as ww
+    join admins as a
+    on ST_Intersects(a.geom, ww.geom)
+  ;
+
 
 create index if not exists ww_rank_in_a__a_ogc_fid on ww_in_admin_ranks (a_ogc_fid);
 create index if not exists ww_rank_in_a__ww_ogc_fid on ww_in_admin_ranks (ww_ogc_fid);
